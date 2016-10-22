07855e74d8f0f123ab826a07c9f015191926f2d7
Stephen Lindberg <stwelin@gmail.com>
2016-10-22 18:48:43 -0400

    things

--- Noseferatu/Assets/Scripts/ObstacleSpawner.cs
+++ Noseferatu/Assets/Scripts/ObstacleSpawner.cs
@@ -8,6 +8,9 @@ public class ObstacleSpawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         InvokeRepeating ("Spawn", 2, 2.5f);
+        InvokeRepeating ("Spawn", 2, 2.5f);
+
+        InvokeRepeating ("Spawn", 1, 2.5f);
 	}
 	
 	// Update is called once per frame
@@ -17,7 +20,6 @@ public class ObstacleSpawner : MonoBehaviour {
 
     void Spawn(){
         float n = Camera.main.orthographicSize * 0.9f;
-        Instantiate (obsPrefab, transform.position + Vector3.up * Random.Range (-n, n), Quaternion.identity);
         Instantiate (obsPrefab, transform.position + Vector3.up * Random.Range (-n, n), Quaternion.identity);
     }
 }
---
+++ Noseferatu/Assets/Scripts/Obstacles/Knife.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Knife : Obstacle {
+
+
+	// Use this for initialization
+	void Start () {
+        rb.AddTorque (200);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
