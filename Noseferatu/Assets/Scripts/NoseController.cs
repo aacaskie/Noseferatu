912d6c3a71d5b49ba54ce98a9c8099374851e675
Stephen Lindberg <stwelin@gmail.com>
2016-10-22 17:44:07 -0400

    moving sprites into sprites folder

--- Noseferatu/Assets/Scripts/NoseController.cs
+++ Noseferatu/Assets/Scripts/NoseController.cs
@@ -4,14 +4,18 @@ using UnityEngine.Events;
 
 public class NoseController : MonoBehaviour {
 
+    public GameObject noseBase;
+    public GameObject noseMiddle;
+    public GameObject noseTip;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate () {
+        noseTip.transform.localPosition = noseMiddle.transform.localPosition + new Vector3(noseMiddle.GetComponentInChildren<BoxCollider2D>().bounds.size.x,0,0);
 	}
 
     public void Rotate(Vector2 target){
@@ -24,23 +28,26 @@ public class NoseController : MonoBehaviour {
     public void Extend(Vector2 target){
         Rotate (target);
         //itween shit
-        iTween.Stop(gameObject, "retract");
-        iTween.ScaleTo (gameObject, iTween.Hash (
+        iTween.Stop(noseMiddle.gameObject, "retract");
+        iTween.ScaleTo (noseMiddle.gameObject, iTween.Hash (
             "name", "extend",
             "x", 2f,
             "time", 0.3f,
             "easetype", "spring",
-            "oncomplete", "retract"
+            "oncomplete", "retract",
+            "oncompletetarget", this.gameObject
         ));
 
     }
 
     private void retract(){
-        iTween.ScaleTo (gameObject, iTween.Hash (
+        iTween.ScaleTo (noseMiddle.gameObject, iTween.Hash (
             "name", "retract",
             "x", 0.3f,
             "time", 0.5f,
             "easetype", "easeOutcubic"
         ));
     }
+
+
 }
---
+++ Noseferatu/Assets/ignoreParentScale.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ignoreParentScale : MonoBehaviour {
+
+    private Vector3 initScale;
+	// Use this for initialization
+	void Awake () {
+	    //grab initial local scale
+        initScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (transform.parent) {
+            transform.localScale = new Vector3 (
+                initScale.x / transform.parent.localScale.x,
+                initScale.y / transform.parent.localScale.y,
+                1);
+        }
+	}
+}
