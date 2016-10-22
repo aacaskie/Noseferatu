e002de530c07b25e2ceae82ab691071fbedcc745
Stephen Lindberg <stwelin@gmail.com>
2016-10-22 18:35:06 -0400

    Adding basic obstacle

---
+++ Noseferatu/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+    private Collider2D _collider;
+    public Collider2D collider {
+        get{ return _collider ?? (_collider = GetComponentInChildren<Collider2D> ()); }
+    }
+
+    private Rigidbody2D _rb;
+    public Rigidbody2D rb {
+        get{ return _rb ?? (_rb = GetComponentInChildren<Rigidbody2D> ()); }
+    }
+
+    public float speed;
+    public float life;
+
+	// Use this for initialization
+	void Awake () {
+        rb.gravityScale = 0f;
+        rb.AddForce (Vector2.left * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //countdown and destroy
+        life -= Time.deltaTime;
+        if (life < 0) {
+            Destroy (this.gameObject);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D trigger){
+        if (trigger.CompareTag ("nose")) {
+            rb.AddForce (Vector3.right * speed * 2);
+            rb.AddTorque (-100);
+        }
+
+    }
+}
---
+++ Noseferatu/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+    public GameObject obsPrefab;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating ("Spawn", 2, 2.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Spawn(){
+        float n = Camera.main.orthographicSize * 0.9f;
+        Instantiate (obsPrefab, transform.position + Vector3.up * Random.Range (-n, n), Quaternion.identity);
+        Instantiate (obsPrefab, transform.position + Vector3.up * Random.Range (-n, n), Quaternion.identity);
+    }
+}
